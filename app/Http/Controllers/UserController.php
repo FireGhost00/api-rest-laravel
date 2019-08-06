c30135fe903146b7226e41cfaff72aad64dcf367
Nelson <n00gomez00@gmail.com>
2019-08-05 18:07:28 -0600

    registro de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
@@ -11,8 +12,79 @@ class UserController extends Controller
     }
     public function register(Request $request){
 
-        return "Accion de Registro de usuarios";
+        // Recoger los datos del usuario por POST
+
+        $json = $request->input('json',null);
+
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true); //array
+     
+
+        if(!empty($params_array) && !empty($params)){
+        //limpiar datos
+
+        $params_array = array_map('trim', $params_array);
+
+
+        //validaf datos
+
+        $validate = \Validator::make($params_array,[
+            'name'       => 'required|alpha',
+            'surname'    => 'required|alpha',
+            'email'      => 'required|email|unique:users',
+            'password'   => 'required'
+        ]);
+
+        if($validate->fails()){
+            // validacion fallo
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'El usuario no se ha creado',
+                'errors'  => $validate->errors()
+            );
+            
+        }else{
+            // validacion pasada correctamente
+
+
+            //cifrar la contraseña
+          $pwd =  password_hash($params->password,PASSWORD_BCRYPT,['cost'=>4]);
+         
+
+            //crear el usuario
+
+            $user = new User();
+            $user ->name = $params_array['name'];
+            $user ->surname = $params_array['surname'];
+            $user ->email = $params_array['email'];
+            $user ->password = $pwd;
+            $user ->role = 'ROLE_USER';
+
+            // Guardar el usuario
+
+            $user->save();
+           
+
+            $data = array(
+                'status' => 'succes',
+                'code' => 200,
+                'message' => 'El usuario se creo correctamente',
+                'user' => $user
+                
+            ); 
+        }
+    }else{
+        $data = array(
+            'status' => 'error',
+            'code' => 404,
+            'message' => 'Los datos enviados no son correctos'
+        );  
     }
+
+       return response()->json($data,$data['code']);
+    }
+
     public function login(Request $request){
         return "Accion de login de usuarios";
     }

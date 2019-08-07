83eba609d2201b79a7d5bebf6aafe38e7531afa9
Nelson <n00gomez00@gmail.com>
2019-08-07 16:46:31 -0600

    creacion del update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,92 +8,182 @@ use App\User;
 
 class UserController extends Controller
 {
-    public function pruebas(Request $request){
+    public function pruebas(Request $request)
+    {
         return "Accion de pruebas de USER_CONTROLLER";
     }
-    public function register(Request $request){
+
+
+    public function register(Request $request)
+    {
 
         // Recoger los datos del usuario por POST
 
-        $json = $request->input('json',null);
+        $json = $request->input('json', null);
 
         $params = json_decode($json); //objeto
         $params_array = json_decode($json, true); //array
-     
-
-        if(!empty($params_array) && !empty($params)){
-        //limpiar datos
-
-        $params_array = array_map('trim', $params_array);
 
 
-        //validaf datos
+        if (!empty($params_array) && !empty($params)) {
+            //limpiar datos
 
-        $validate = \Validator::make($params_array,[
-            'name'       => 'required|alpha',
-            'surname'    => 'required|alpha',
-            'email'      => 'required|email|unique:users',
-            'password'   => 'required'
-        ]);
+            $params_array = array_map('trim', $params_array);
 
-        if($validate->fails()){
-            // validacion fallo
+
+            //validaf datos
+
+            $validate = \Validator::make($params_array, [
+                'name'       => 'required|alpha',
+                'surname'    => 'required|alpha',
+                'email'      => 'required|email|unique:users',
+                'password'   => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                // validacion fallo
+                $data = array(
+                    'status' => 'error',
+                    'code' => 404,
+                    'message' => 'El usuario no se ha creado',
+                    'errors'  => $validate->errors()
+                );
+            } else {
+                // validacion pasada correctamente
+
+
+                //cifrar la contraseña
+                $pwd =  hash('sha256', $params->password);
+
+
+                //crear el usuario
+
+                $user = new User();
+                $user->name = $params_array['name'];
+                $user->surname = $params_array['surname'];
+                $user->email = $params_array['email'];
+                $user->password = $pwd;
+                $user->role = 'ROLE_USER';
+
+                // Guardar el usuario
+
+                $user->save();
+
+
+                $data = array(
+                    'status' => 'succes',
+                    'code' => 200,
+                    'message' => 'El usuario se creo correctamente',
+                    'user' => $user
+
+                );
+            }
+        } else {
             $data = array(
                 'status' => 'error',
                 'code' => 404,
-                'message' => 'El usuario no se ha creado',
-                'errors'  => $validate->errors()
+                'message' => 'Los datos enviados no son correctos'
             );
-            
-        }else{
-            // validacion pasada correctamente
-
-
-            //cifrar la contraseña
-          $pwd =  hash('sha256',$params->password);
-         
-
-            //crear el usuario
-
-            $user = new User();
-            $user ->name = $params_array['name'];
-            $user ->surname = $params_array['surname'];
-            $user ->email = $params_array['email'];
-            $user ->password = $pwd;
-            $user ->role = 'ROLE_USER';
-
-            // Guardar el usuario
-
-            $user->save();
-           
-
-            $data = array(
-                'status' => 'succes',
-                'code' => 200,
-                'message' => 'El usuario se creo correctamente',
-                'user' => $user
-                
-            ); 
         }
-    }else{
-        $data = array(
-            'status' => 'error',
-            'code' => 404,
-            'message' => 'Los datos enviados no son correctos'
-        );  
+
+        return response()->json($data, $data['code']);
     }
 
-       return response()->json($data,$data['code']);
-    }
-
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $jwtAuth = new \JwtAuth();
 
-        $email = 'ngom@gmail.com';
-        $password = 'nelson';
-        $pwd =  hash('sha256', $password);
+        // Recibir  datos por Post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        // Validar datos 
+        $validate = \Validator::make($params_array, [
+
+            'email'      => 'required|email',
+            'password'   => 'required'
+        ]);
+
+        if ($validate->fails()) {
+            // validacion fallo
+            $singup = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'El usuario no se ha podido identificar',
+                'errors'  => $validate->errors()
+            );
+        } else {
+            // cifrar la password
+            $pwd =  hash('sha256', $params->password);
+            // devolver token o datos
+            $singup =  $jwtAuth->signup($params->email, $pwd);
+            if (!empty($params->gettoken)) {
+                $singup =  $jwtAuth->signup($params->email, $pwd, true);
+            }
+        }
 
 
-        return response()->json( $jwtAuth->signup($email,$pwd,true),200);
+
+        return response()->json($singup, 200);
+    }
+
+
+    public function update(Request $request)
+    {
+        // Comprobar si el usuario esta identificado
+        $token = $request->header('Authorization');
+        $jwtAuth = new \JwtAuth();
+        $checkToken = $jwtAuth->checkToken($token);
+
+             
+            // recoger los datos por post
+
+            $json = $request->input('json', null);
+            $params_array = json_decode($json, true);
+            // $json = $request->input('json', null);
+
+            // $params_array = json_decode($json, true);
+
+        if ($checkToken && !empty($params_array)) {
+         
+           
+            //sacar usuario identificado
+            $user = $jwtAuth->checkToken($token, true);
+            // validar los datos
+            $validate =  \Validator::make($params_array, [
+                'name'       => 'required|alpha',
+                'surname'    => 'required|alpha',
+                'email'      => 'required|email|unique:users,'.$user->sub
+                
+            ]);
+
+            // Quitar los datos que no quiero actualizar
+                unset($params_array['id']);
+                unset($params_array['role']);
+                unset($params_array['password']);
+                unset($params_array['create_at']);
+                unset($params_array['remember_token']);
+       
+            // Actualizar usuario en bd
+                $user_update = User::Where('id',$user->sub)->update($params_array);
+
+            // Devolver array con resultado
+
+            $data = array(
+                'code' => 200,
+                'status' => 'success',
+                'user' => $user,
+                'change' => $params_array 
+            );
+
+        } else {
+           $data = array(
+               'code' => 400,
+               'status' => 'error',
+               'message' => 'El usuario no esta identificado.'
+           );
+        }
+        return response()->json($data, $data['code']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,4 @@ Route::get('/test-orm','PruebasController@testOrm');
        // Rutas del controlador de usuarios
        Route::post('/api/register', 'UserController@register');
        Route::post('/api/login', 'UserController@login');
+       Route::put('/api/user/update', 'UserController@update');
